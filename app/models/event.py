09fc6d4a81f19cef9d907b766752414de836f2c6
Alex Carl <alexjcarl97@gmail.com>
2024-10-08 21:36:49 -0400

    deploy

--- app/models/event.py
+++ app/models/event.py
@@ -5,7 +5,7 @@ from .db import db, environment, SCHEMA, add_prefix_for_prod
 class Event(db.Model):
     __tablename__ = 'events'
 
-    if environment == 'production':
+    if environment == 'production': 
         __table_args__ = {'schema': SCHEMA}
 
     id = db.Column(db.Integer, primary_key=True, nullable=False)

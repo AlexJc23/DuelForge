f2f4b32f448afcf9b11258ce2d74407ee8397f1d
Alex Carl <alexjcarl97@gmail.com>
2024-10-10 23:15:18 -0400

    update signup route

--- app/forms/signup_form.py
+++ app/forms/signup_form.py
@@ -2,6 +2,7 @@ from flask_wtf import FlaskForm
 from wtforms import StringField
 from wtforms.validators import DataRequired, Email, ValidationError
 from app.models import User
+import re
 
 
 def user_exists(form, field):
@@ -19,9 +20,19 @@ def username_exists(form, field):
     if user:
         raise ValidationError('Username is already in use.')
 
+def CheckUrlImage(form, field):
+    image_url = field.data
+    # Regular expression to check for valid image extensions
+    valid_extensions = ('.jpg', '.jpeg', '.png')
+    if not any(image_url.endswith(ext) for ext in valid_extensions):
+        raise ValidationError('Image URL must be in .jpg, .jpeg, or .png format.')
+
 
 class SignUpForm(FlaskForm):
     username = StringField(
         'username', validators=[DataRequired(), username_exists])
     email = StringField('email', validators=[DataRequired(), user_exists])
+    first_name = StringField('first_name', validators=[DataRequired()])
+    last_name = StringField('last_name', validators=[DataRequired()])
+    image_url = StringField('image_url', validators=[CheckUrlImage])
     password = StringField('password', validators=[DataRequired()])

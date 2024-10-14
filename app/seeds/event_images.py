07e437bf4a4d2624cc7742903095a4b1410e6e1b
Alex Carl <alexjcarl97@gmail.com>
2024-10-14 18:34:16 -0400

    homepage update and seeddata update

--- app/seeds/event_images.py
+++ app/seeds/event_images.py
@@ -3,11 +3,46 @@ from sqlalchemy.sql import text
 
 
 def seed_eventImage():
+    image_urls = [
+        'https://i.redd.it/rhpkbahwt5ic1.jpeg',
+        'https://yugiohnz.com/wp-content/uploads/2022/04/states-2022-trophy-600x450-1.jpeg',
+        'https://images.pexels.com/photos/20131195/pexels-photo-20131195/free-photo-of-iphone-with-the-game-yu-gi-oh-duel-links-playing-on-the-display-an-ipad-playing-yu-gi-oh-card-game-the-chronicles-yu-gi-oh-card-game-25th-anniversary-jovan-vasiljevic-photography.jpeg',
+        'https://static1.cbrimages.com/wordpress/wp-content/uploads/2020/07/Yugioh-Duel-Disk.jpeg.jpg',
+        'https://www.shutterstock.com/image-photo/guanajuato-mexico-february-22-2021-260nw-1922447285.jpg',
+        'https://shared.akamai.steamstatic.com/store_item_assets/steam/apps/1449850/capsule_616x353.jpg',
+        'https://roadoftheking.com/wp-content/uploads/2024/09/YuGiOhWorldChampionship.jpg',
+        'https://i.ytimg.com/vi/Ve-7p60raFU/maxresdefault.jpg',
+        'https://i.ytimg.com/vi/I5iKzm1i6VE/maxresdefault.jpg',
+        'https://pm1.aminoapps.com/7489/15463377b010b2c0a1d95af685cdadf7da69f2d6r1-1080-374v2_uhq.jpg',
+        'https://pbs.twimg.com/media/GFFQsMAaIAAtb6X.jpg',
+        'https://www.heypoorplayer.com/wp-content/uploads/2017/02/YGOmain.jpg',
+        'https://assetsio.gnwcdn.com/yu-gi-oh-master-duel-gameplay.png',
+        'https://static1.srcdn.com/wordpress/wp-content/uploads/2022/01/YGO-Master-Duel-Fusion.jpg',
+        'https://i.ytimg.com/vi/QrHRMrWJ9Lk/hq720.jpg',
+        'https://img.freepik.com/premium-photo/nighttime-scene-with-starry-background_410516-18837.jpg',
+        'https://yugiohblog.konami.com/wp-content/uploads/2018/02/UDS-Belt.jpg',
+        'https://i.redd.it/435wlf313uh21.jpg',
+        'https://bloximages.newyork1.vip.townnews.com/dailynebraskan.com/content/tncms/assets/v3/editorial/0/4f/04f86ee8-3870-11ed-a65b-b748990239fc/6328f6a272314.image.jpg',
+        'https://c8.alamy.com/comp/DYCT8W/teenage-boys-playing-a-yu-gi-oh-trading-card-tournament-DYCT8W.jpg',
+        'https://static1.cbrimages.com/wordpress/wp-content/uploads/2020/07/yugioh.jpg',
+        'https://i.ytimg.com/vi/m1SCk4Lkuy8/maxresdefault.jpg',
+        'https://i.ytimg.com/vi/VqUQqrbyHc0/hqdefault.jpg',
+        'https://i.ytimg.com/vi/xnFWO61D91k/maxresdefault.jpg',
+        'https://i.ytimg.com/vi/5-VMhDb4ahE/maxresdefault.jpg',
+        'https://i.ytimg.com/vi/mQ0S-pnJp9Y/sddefault.jpg',
+        'https://i.redd.it/vrfo0gv5u5wb1.jpg',
+        'https://i.ytimg.com/vi/bBhdKH9zUxM/maxresdefault.jpg',
+        'https://64.media.tumblr.com/58040c93c036839816b0d5359b9dec9f/tumblr_inline_oz8sy8qaKF1qc7qsj_540.jpg',
+        'https://i.ebayimg.com/images/g/NVUAAOSwDFhgs37r/s-l1200.jpg',
+    ]
 
-    eventimage_1 = EventImage(event_id=1, image_url='http://example.jpg')
+    event_images = []
+    for i, url in enumerate(image_urls, start=1):
+        event_images.append(EventImage(event_id=i, image_url=url))
 
-    db.session.add(eventimage_1)
+    db.session.bulk_save_objects(event_images)
     db.session.commit()
+
 
 def undo_eventImages():
     if environment == "production":
--- react-vite/src/components/HomePage/HomePage.jsx
+++ react-vite/src/components/HomePage/HomePage.jsx
@@ -1,11 +1,88 @@
-
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getDecks } from "../../redux/decks";
+import { NavLink } from 'react-router-dom';
+import { FaArrowRight } from "react-icons/fa";
+import './HomePage.css';
+import { getEvents } from "../../redux/events";
 
 const HomePage = () => {
+    const dispatch = useDispatch();
+    const decks = useSelector(state => state.decksReducer.allDecks);
+    const allDecks = decks ? Object.values(decks) : [];
+
+    const events = useSelector(state => state.eventsReducer.allEvents)
+    const allEvents = events ? Object.values(events) : [];
+
+
+    // Function to get random decks
+    const getRandomStuff = (decksArray, numberOfDecks = 4) => {
+        const copyDecksArray = [...decksArray];
+        for (let i = copyDecksArray.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [copyDecksArray[i], copyDecksArray[j]] = [copyDecksArray[j], copyDecksArray[i]];
+        }
+        return copyDecksArray.slice(0, numberOfDecks);
+    };
+
+    // Get 3 random decks
+    const randomDecks = getRandomStuff(allDecks);
+    const randomEvents = getRandomStuff(allEvents)
+    console.log(randomEvents)
+
+    // Dispatch action to fetch decks on component mount
+    useEffect(() => {
+        dispatch(getDecks());
+        dispatch(getEvents())
+    }, [dispatch]);
+
+    // loading screen
+    if (!allDecks.length) return (<div id="loading"><h1 className='loading'>
+        <span class="let1">l</span>
+        <span class="let2">o</span>
+        <span class="let3">a</span>
+        <span class="let4">d</span>
+        <span class="let5">i</span>
+        <span class="let6">n</span>
+        <span class="let7">g</span>
+    </h1></div>);
+
     return (
-        <h1>
-            Hello World!
-        </h1>
-    )
-}
+        <div className="home-content">
+            <div className="deck-featured">
+                <h3 >Featured Decks <span className="featured-line" ></span></h3>
+                <ul className="stuff">
+                    {randomDecks.map((successfulDeck) => (
+                        <NavLink key={successfulDeck.id} to={`/decks/${successfulDeck.id}`} className="bubbles">
+                            {successfulDeck.cards && successfulDeck.cards.length > 0 && successfulDeck.cards[0].images.length > 0 ? (
+                                <div className="card">
+                                    {/* Displaying overlapping deck images */}
+                                    <img className="deck-img image1" src={successfulDeck.cards[0].images[0].image_url} alt="deck card 1" />
+                                    {successfulDeck.cards[1] && (
+                                        <img className="deck-img image2" src={successfulDeck.cards[1].images[0].image_url} alt="deck card 2" />
+                                    )}
+                                    {successfulDeck.cards[2] && (
+                                        <img className="deck-img image3" src={successfulDeck.cards[2].images[0].image_url} alt="deck card 3" />
+                                    )}
+                                </div>
+                            ) : (
+                                <div className="no-image">No image available</div>
+                            )}
+                            <p style={{marginTop: '19px', marginLeft: '16px'}}>{successfulDeck.name}</p>
+                        </NavLink>
+                    ))}
+                    <div style={{}} class="glow-on-hover">
+                    <NavLink to={'/decks'} class="view-btn">View all Decks <FaArrowRight/></NavLink>
+
+                    </div>
+                </ul>
+            </div>
+            <div className="event-featured">
+                <h3 >Featured Decks <span className="featured-line" ></span></h3>
+
+            </div>
+        </div>
+    );
+};
 
 export default HomePage;
